19eecadbd4d793ed7ef8798af6961d99af837b21
Alex Guanipatin <sebas5950@gmail.com>
2022-05-23 11:16:30 -0400

    commit tree 96e389ab4f9c6a257e44af3e54b035ea86799096

--- index.js
+++ index.js
@@ -1,2 +1,50 @@
-function addingEventListener() {
+ 
+ const alertButton = document.querySelector('#input')
+
+function  addingEventListener() {
+    alertButton.addEventListener('click', () => alert('I was clicked'))
 }
+ 
+addingEventListener()
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ // const input = document.getElementById("input");
+
+// function addingEventListener() {
+//     input.addEventListener('click', function() {
+//         alert('I was clicked!');
+//       });
+//   }
